f0854fe95ffed138299e1cd1f0a16a38c769a886
Meriam Ayadi <ayedi.mariem1997@gmail.com>
2026-03-17 16:35:16 +0100

    feat(todos): add css only filters

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,7 +1,7 @@
 <template>
   <AwesomeArticle>
     <h1>User Todo's</h1>
-    <div>
+    <div class="todo-filters">
       Filters:
       <span>
         <label for="completed">Show completed</label>
@@ -9,17 +9,33 @@
       </span>
       <span>
         <label for="pending">Show pending</label>
-        <input type="checkbox" id="pending" />
+        <input type="checkbox" id="pending" checked />
       </span>
     </div>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <h4>{{ todo.title }}</h4>
+    <ul class="todo-list">
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        :completion-status="todo.completed ? 'completed' : 'pending'"
+      >
+        <h4 :title="todo.title">{{ todo.title }}</h4>
         <p>Status: {{ todo.completed ? 'Completed' : 'Pending' }}</p>
       </li>
     </ul>
   </AwesomeArticle>
 </template>
+
+<style scoped>
+/* when show completed is unchecked we hide completed todos. */
+.todo-filters:has(#completed:not(:checked)) + .todo-list li[completion-status='completed'] {
+  display: none;
+}
+
+/* when show pending is unchecked we hide pending todos. */
+.todo-filters:has(#pending:not(:checked)) + .todo-list li[completion-status='pending'] {
+  display: none;
+}
+</style>
 
 <script setup>
 const route = useRoute();
